066ee6a6eff059982fca070ed956268f54ed252a
miladiashe <dkdud0122@naver.com>
2021-04-13 00:29:12 +0900

    help

--- TEST_KIM/main.py
+++ TEST_KIM/main.py
@@ -1,10 +1,18 @@
 #%%
 import librosa
 import matplotlib.pyplot as plt
+import IPython.display
 
 audio_path = 'testdata1.wav'
 
 w, sr = librosa.load(audio_path)
 
 print(plt.plot(audio_path[0:10]))
+
+#%%
+IPython.display.Audio('bensound-theelevatorbossanova.mp3')
+
+#출처: https://banana-media-lab.tistory.com/entry/Librosa-python-library로-음성파일-분석하기 [Banana Media Lab]
+
+
 # %%
